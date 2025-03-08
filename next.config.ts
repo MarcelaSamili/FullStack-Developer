1f130cd6e583cc45808c99a68857e2ecb7a1197e
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-03-08 15:11:17 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 //** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export', // Permite gerar arquivos estáticos
   images: {
     unoptimized: true, // Necessário para exportação estática
   },
-  basePath: isProd ? '/FullStack-Developer' : '', // Use o nome do repositório
-  assetPrefix: isProd ? '/FullStack-Developer' : '',
+  typescript: { ignoreBuildErrors: true },
   eslint: {
     ignoreDuringBuilds: true,
   },
